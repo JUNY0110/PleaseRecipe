ee3995bfcc54fbb9ec8171b827b253b760d140d2
Juny0110 <wnsdyds403@naver.com>
2024-06-01 17:10:45 +0900

    Resource #42: Color 수정

---
+++ PleaseRecipe/Resources/Constants/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  PleaseRecipe
+//
+//  Created by 지준용 on 2023/10/10.
+//
+
+import UIKit
+
+enum Color {
+    static let mainRed = UIColor(resource: .mainRed)
+    static let storageRed = UIColor(resource: .storageRed)
+    static let storageGreen = UIColor(resource: .storageGreen)
+    static let storageBlue = UIColor(resource: .storageBlue)
+}
