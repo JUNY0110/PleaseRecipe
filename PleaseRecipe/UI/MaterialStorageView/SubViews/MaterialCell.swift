6e0cfb6415d79616e6772616a58ad6c1f28e85f1
Juny0110 <wnsdyds403@naver.com>
2024-06-05 17:11:48 +0900

    Feat #42: 화면에 따른 Cell 재사용을 위한 enum 추가
    - 재료창고, 재료추가, 재료등록 화면에 따른 enum을 통한 설정 적용
    - enum을 활용한 cell 재사용성 향상

--- PleaseRecipe/UI/MaterialStorageView/SubViews/MaterialCell.swift
+++ PleaseRecipe/UI/MaterialStorageView/SubViews/MaterialCell.swift
@@ -9,12 +9,33 @@ import UIKit
 
 import SnapKit
 
+enum PageType {
+    case storage
+    case addtion
+    case regist
+}
 
 final class MaterialCell: UICollectionViewCell {
     
     // MARK: - Properities
     static let identifier = "MaterialCell"
     var image: UIImage?
+    
+    private var pageType: PageType = .storage {
+        didSet {
+            // TODO: isHidden을 다시 안 바꿔도 괜찮은지 확인을 위한 TODO 주석
+            switch pageType {
+            case .storage:
+                break
+            case .addtion:
+                imageView.isHidden = true
+                nameLabel.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+            case .regist:
+                nameLabel.isHidden = true
+            }
+        }
+    }
+    
     override var isSelected: Bool {
         didSet {
             configureSelected(isSelected)
@@ -29,13 +50,13 @@ final class MaterialCell: UICollectionViewCell {
         return $0
     }(UIStackView())
     
-    private let imageView: UIImageView = {
+    private lazy var imageView: UIImageView = {
         $0.image = UIImage(systemName: "plus")
         $0.contentMode = .scaleAspectFit
         return $0
     }(UIImageView())
     
-    private let nameLabel: UILabel = {
+    private lazy var nameLabel: UILabel = {
         $0.text = "테스트"
         $0.textAlignment = .center
         $0.font = .systemFont(ofSize: 12, weight: .regular)
@@ -65,9 +86,11 @@ final class MaterialCell: UICollectionViewCell {
     // MARK: - Attribute
     private func attribute() {
         self.layer.cornerRadius = 8
+        self.layer.borderColor = UIColor.secondarySystemBackground.cgColor
+        self.layer.borderWidth = 1
     }
     
-    // MARK: - layout
+    // MARK: - Layout
     private func addSubViews() {
         contentView.addSubview(vStackView)
         vStackView.addArrangedSubview(imageView)
@@ -83,20 +106,21 @@ final class MaterialCell: UICollectionViewCell {
 
 // MARK: - Configure
 extension MaterialCell {
-    func configureCell(image: UIImage?, name: String = "") {
+    func configureCell(image: UIImage?, name: String = "", pageType: PageType) {
         imageView.image = image
         nameLabel.text = name
+        self.pageType = pageType
     }
     
-    private func configureSelected(_ selected: Bool) {
+    func configureSelected(_ selected: Bool) {
         if selected {
-            layer.borderColor = .init(red: 255/255, green: 148/255, blue: 148/255, alpha: 1)
+            layer.borderColor = UIColor.mainRed.cgColor
             layer.borderWidth = 3
             
             image = imageView.image
         } else {
-            layer.borderColor = .init(red: 255/255, green: 148/255, blue: 148/255, alpha: 0)
-            layer.borderWidth = .zero
+            layer.borderColor = UIColor.secondarySystemBackground.cgColor
+            layer.borderWidth = 1
         }
     }
 }
