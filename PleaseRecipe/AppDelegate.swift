00b6074c5c70e7e2b886fa5e9d413fb37104a5b9
Juny0110 <wnsdyds403@naver.com>
2024-06-30 02:56:37 +0900

    Feat #44: Coredata 앱 실행 시 메모리에 올리기 위한 작업

--- PleaseRecipe/AppDelegate.swift
+++ PleaseRecipe/AppDelegate.swift
@@ -6,10 +6,12 @@
 //
 
 import UIKit
+import CoreData
 
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        _ = CoreDataManager.shared
         
         return true
     }
